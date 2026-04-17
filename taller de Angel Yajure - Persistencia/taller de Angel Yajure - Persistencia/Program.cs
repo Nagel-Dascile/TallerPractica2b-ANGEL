f83824fb6ace47982961abd4e182e3d02da80502
Nagel-Dascile <nglyajure@gmail.com>
2026-04-17 14:53:55 -0400

    aqui esta lo de los directorios

--- taller de Angel Yajure - Persistencia/taller de Angel Yajure - Persistencia/Program.cs
+++ taller de Angel Yajure - Persistencia/taller de Angel Yajure - Persistencia/Program.cs
@@ -1,5 +1,12 @@
-﻿
+﻿/*
+ * 
+ * Usuario - Angel Yajure
+ * 
+ * 
+/*/
+
 using System;
+using System.IO;
 
 namespace taller_de_Angel_Yajure___Persistencia
 {
@@ -7,8 +14,19 @@ namespace taller_de_Angel_Yajure___Persistencia
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Maldito desgraciado te odio y te detesto, eres tan malo que hasta Araujo ni te hace una roja en Champions");
-
+			Console.WriteLine("----=== Taller Seccion B ===----");
+			
+			string rutaRaiz = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DatosdelIUJO");
+			string ReportsRoute = Path.Combine(rutaRaiz, "Reportes");
+			
+			Console.WriteLine(rutaRaiz);
+			Console.WriteLine(ReportsRoute);
+			
+			if (!Directory.Exists(ReportsRoute))
+			{
+				Directory.CreateDirectory(ReportsRoute);
+				Console.WriteLine("Se ha creado un directorio satisfactoriamente");
+			}
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
